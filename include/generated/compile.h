f150be20063f7bb4e64a7891742bc77f28d87d4e
may21 <may21.lee@gmail.com>
2021-12-30 15:42:33 +0900

    source upload

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#2 SMP Wed Dec 15 18:12:22 KST 2021"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "lvhost"
+#define LINUX_COMPILER "gcc version 7.5.0 (Ubuntu 7.5.0-3ubuntu1~18.04)"
